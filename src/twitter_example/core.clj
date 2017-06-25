ea7d1f657759b7972eb1074e13d66e83738e99ab
feychou <federica.recanatini@gmail.com>
2017-06-25 10:37:23 +0200

    Multiple blanks

--- src/twitter_example/core.clj
+++ src/twitter_example/core.clj
@@ -16,26 +16,40 @@
 ; We define the "templates" - these strings are the skeleton of the tweet
 ; We will later replace every occurence of ___ with a string that we chose
 ; randomly from the list "blanks".
-(def templates ["I hear he eats ___"
-                "The police arrested ___ in connection with the robbery."
-                "I don't know if ___ will visit us next Sunday."
-                "She sent a card to ___."
-                "Doing that sort of thing makes you look like ___."])
+(def templates [
+                "___ sent a card to ___."])
 
 ; Next we define the "blanks"
-(def blanks ["a Haunted House"
+(def blanks ["a haunted house"
              "Rich Hickey"
              "a purple dinosaur"
-             "Junk Mail"
-             "Pirates"])
+             "junk mail"
+             "pirates"])
+
+; We are using build-sentence in generate-sentence, so we need to declare it
+; since we define it only afterwards
+(declare build-sentence)
 
 ; generate-sentence returns a random sentence, built by choosing one template
-; string at random and filling in the blank space (___) with a randomly chosen
+; string at random and filling in each blank space (___) with a randomly chosen
 ; string from the blanks list.
+; Implementation: We take a random template string, and prepare the available
+; blanks by shuffling them. We then hand over to build-sentence.
 (defn generate-sentence []
-  (let [template (rand-nth templates)
-        blank (rand-nth blanks)]
-      (s/replace template "___" blank)))
+  (build-sentence (rand-nth templates) (shuffle blanks)))
+
+; build-sentence is used by generate-sentence to build up a sentence from a
+; template step by step.
+; Starting with the template given to us by generate-sentence we replace
+; one occurence of ___ with the first strings in blanks. Next, we check
+; if there are more ___ remaining, if so we do it again. Otherwise we're done.
+(defn build-sentence [template blanks]
+  (let [blank (first blanks)
+        remaining-blanks (rest blanks)
+        sentence (s/replace-first template "___" blank)]
+      (if (s/includes? sentence "___")
+        (recur sentence remaining-blanks)
+        sentence)))
 
 ; We retrieve the twitter credentials from the profiles.clj file here.
 ; In profiles.clj we defined the "env(ironment)" which we use here
